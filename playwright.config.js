b0950196c41f65b8b997055f326522b9c4d2da83
Philip Bontrager <pbontrager@meta.com>
2026-09-01 07:53:53 -0400

    test: retain browser test videos

--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,7 @@ module.exports = {
   use: {
     ...base.use,
     baseURL: 'http://127.0.0.1:9988',
+    video: 'on',
     trace: 'retain-on-failure',
     launchOptions: process.env.PLAYWRIGHT_EXECUTABLE_PATH
       ? { executablePath: process.env.PLAYWRIGHT_EXECUTABLE_PATH }
